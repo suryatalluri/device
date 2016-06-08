9367b5d27b9eef2ad4646ca88b82f1165748341f
root <root@5b239abaf6dd>
2016-06-08 07:10:21 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,10 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "defaultLanguage" : "en",
+  "displayName" : "Device",
+  "homePage" : "Main",
+  "name" : "device",
+  "platformType" : "MOBILE",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
